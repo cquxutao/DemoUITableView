ed8c6de8a75183869a536f2a0d45f20025c8df28
cquxutao <178052394@qq.com>
2017-07-05 20:29:38 +0800

    Wednesday, 5 July 2017 at 20:29:34

---
+++ DemoUITableView/DemoUITableView/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  DemoUITableView
+//
+//  Created by 徐涛 on 05/07/2017.
+//  Copyright © 2017 徐涛. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DemoUITableView/DemoUITableView/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  DemoUITableView
+//
+//  Created by 徐涛 on 05/07/2017.
+//  Copyright © 2017 徐涛. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
